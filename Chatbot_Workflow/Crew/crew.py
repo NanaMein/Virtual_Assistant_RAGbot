d000b62df7ebd8dbc0d43f3137cfe0bf62739057
Alfeo The Third <alfeoephraimquintay@gmail.com>
2025-07-07 00:04:54 +0800

    day 1 finished

---
+++ Chatbot_Workflow/Crew/crew.py
@@ -0,0 +1,62 @@
+# import litellm
+# litellm._turn_on_debug()
+import asyncio
+from collections import deque
+from functools import lru_cache
+from typing import Deque, Type, Optional
+from llama_index.core.storage.chat_store.base_db import MessageStatus
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.embeddings.cohere import CohereEmbedding
+from llama_index.vector_stores.milvus import MilvusVectorStore
+from llama_index.vector_stores.milvus.utils import BGEM3SparseEmbeddingFunction
+from llama_index.core import VectorStoreIndex, Document, StorageContext, SimpleDirectoryReader
+from llama_index.core.text_splitter import SentenceSplitter
+from llama_index.core.tools import QueryEngineTool, FunctionTool
+from llama_index.core.agent.workflow import FunctionAgent, ReActAgent, AgentWorkflow
+from llama_index.core.memory.memory import Memory
+from llama_index.core.workflow import Context
+from llama_index.llms.groq import Groq
+from datetime import datetime, timezone, timedelta
+import pytz
+from crewai.flow import Flow, start, listen, router, or_, and_, persist
+# from llama_index.core.base.llms.types import ChatMessage  # , TextBlock
+from llama_index.core.base.llms.base import ChatMessage  # schema import ChatMessage
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+from crewai import LLM, Agent, Task, Crew, Process
+from crewai.tools import tool, BaseTool
+from crewai.project import CrewBase, task, agent, crew
+from cachetools import TTLCache, cached
+import os
+from collections import defaultdict
+import pytz
+from llama_index.core.schema import MetadataMode
+import time
+import grpc
+from pymilvus import connections
+from pymilvus.exceptions import MilvusException
+
+
+load_dotenv()
+
+
+
+class CrewChatbot:
+
+    for_cache = TTLCache(ttl=3600, maxsize=100)
+    vector_cache = TTLCache(ttl=300, maxsize=10)
+
+    def __init__(self, user_id: str):
+        self.user_id: str = user_id
+
+
+    agent_config = "config/agents.yaml"
+    task_config = "config/tasks.yaml"
+
+    @agent
+    def agent(self):
+        return Agent(
+            config=self.agent_config["research_agent"], # type: ignore[index]
+            verbose=True,
+        ) # type: ignore[index]
+
---
+++ Chatbot_Workflow/Flow/flow.py
@@ -0,0 +1,41 @@
+# import litellm
+# litellm._turn_on_debug()
+import asyncio
+from collections import deque
+from functools import lru_cache
+from typing import Deque, Type, Optional
+from llama_index.core.storage.chat_store.base_db import MessageStatus
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.embeddings.cohere import CohereEmbedding
+from llama_index.vector_stores.milvus import MilvusVectorStore
+from llama_index.vector_stores.milvus.utils import BGEM3SparseEmbeddingFunction
+from llama_index.core import VectorStoreIndex, Document, StorageContext, SimpleDirectoryReader
+from llama_index.core.text_splitter import SentenceSplitter
+from llama_index.core.tools import QueryEngineTool, FunctionTool
+from llama_index.core.agent.workflow import FunctionAgent, ReActAgent, AgentWorkflow
+from llama_index.core.memory.memory import Memory
+from llama_index.core.workflow import Context
+from llama_index.llms.groq import Groq
+from datetime import datetime, timezone, timedelta
+import pytz
+from crewai.flow import Flow, start, listen, router, or_, and_, persist
+# from llama_index.core.base.llms.types import ChatMessage  # , TextBlock
+from llama_index.core.base.llms.base import ChatMessage  # schema import ChatMessage
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+from crewai import LLM, Agent, Task, Crew, Process
+from crewai.tools import tool, BaseTool
+from crewai.project import CrewBase, task, agent, crew
+from cachetools import TTLCache, cached
+import os
+from collections import defaultdict
+import pytz
+from llama_index.core.schema import MetadataMode
+import time
+import grpc
+from pymilvus import connections
+from pymilvus.exceptions import MilvusException
+
+
+
+load_dotenv()
