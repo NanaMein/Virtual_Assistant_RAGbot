389242498d04b2357254dbd4627f8a60fd76cd67
Alfeo The Third <alfeoephraimquintay@gmail.com>
2025-07-10 12:54:40 +0800

    successfully finished and tested cache, now able to implement modularity to the main chatbots to cache in memory conversation text

--- Chatbot_Workflow/Groq/groq_chat_cache.py
+++ Chatbot_Workflow/Groq/groq_chat_cache.py
@@ -28,22 +28,22 @@ class GroqChatCache:
             self.groq_cache[memory_cache_id] = memory
         return memory
 
-    def add_user_to_memory_cache(self, user_input_message: str = None):
-        if user_input_message is None:
+    def add_user_to_memory_cache(self, user_input_message: str = ""):
+        if not user_input_message:
+            memory = self._core_memory_cache(self.user_id)
+
+            user_msg_ready = ChatCompletionUserMessageParam(
+                content="*speechless*", role="user"
+            )
+            memory.append(user_msg_ready)
             return False
 
-        memory = self._core_memory_cache(self.user_id)
-        ph_time = datetime.now(pytz.timezone('Asia/Manila')).strftime('%Y-%m-%d %H:%M:%S')
-
-        msg_with_time = f"""<TimeStamp({ph_time})>
-        [content={user_input_message}]
-        </TimeStamp({ph_time})>"""
+        memory1 = self._core_memory_cache(self.user_id)
 
         user_msg_ready = ChatCompletionUserMessageParam(
-            content=msg_with_time, role="user"
+            content=user_input_message, role="user"
         )
-
-        memory.append(user_msg_ready)
+        memory1.append(user_msg_ready)
         return True
 
     def add_assistant_to_memory_cache(self, assistant_input_message: str = None):
@@ -51,14 +51,45 @@ class GroqChatCache:
             return False
         memory = self._core_memory_cache(self.user_id)
 
+        assist_msg_ready = ChatCompletionAssistantMessageParam(
+            content=assistant_input_message, role="assistant"
+        )
+        memory.append(assist_msg_ready)
+        return True
+
+    def add_user_to_memory_with_time(self, user_input_message: str = None):
+        if user_input_message is None:
+            return False
+
+        memory = self._core_memory_cache(self.user_id)
         ph_time = datetime.now(pytz.timezone('Asia/Manila')).strftime('%Y-%m-%d %H:%M:%S')
 
-        msg_with_time = f"""<TimeStamp({ph_time})>
-        [content={assistant_input_message}]
-        </TimeStamp({ph_time})>"""
+        # msg_with_time = f"""<TimeStamp({ph_time})>
+        # [content={user_input_message}]
+        # </TimeStamp({ph_time})>"""
+        msg_with_time1 = f"""{user_input_message}[timestamp({ph_time})]"""
+
+        user_msg_ready = ChatCompletionUserMessageParam(
+            content=msg_with_time1, role="user"
+        )
+
+        memory.append(user_msg_ready)
+        return True
+
+    def add_assistant_to_memory_with_time(self, assistant_input_message: str = None):
+        if assistant_input_message is None:
+            return False
+        memory = self._core_memory_cache(self.user_id)
+
+        ph_time = datetime.now(pytz.timezone('Asia/Manila')).strftime('%Y-%m-%d %H:%M:%S')
+
+        # msg_with_time = f"""<TimeStamp({ph_time})>
+        # [content={assistant_input_message}]
+        # </TimeStamp({ph_time})>"""
+        msg_with_time1 = f"""{assistant_input_message}[timestamp({ph_time})]"""
 
         assist_msg_ready = ChatCompletionAssistantMessageParam(
-            content=msg_with_time, role="assistant"
+            content=msg_with_time1, role="assistant"
         )
 
         memory.append(assist_msg_ready)
@@ -69,8 +100,14 @@ class GroqChatCache:
         new_list = [] + orig_list
         return new_list
 
-    def get_chat_with_system_prompt(self, system_prompt: str):
+    def get_chat_with_system_prompt(self, system_prompt_template: str = ""):
+
+        system_prompt = ChatCompletionSystemMessageParam(
+            content=system_prompt_template, role="system"
+        )
+
         orig_chat = self._core_memory_cache(self.user_id)
+
         system_and_chat = [system_prompt] + orig_chat
         return system_and_chat
 
@@ -82,6 +119,16 @@ class GroqChatCache:
         except Exception as e:
             print(type(e))
             return False
+
+    def auto_delete_with_limiter(self, limit_to_hold: int):
+        memory = self._core_memory_cache(self.user_id)
+        while len(memory) > limit_to_hold:
+            memory.pop(0)
+        return len(memory)
+
+    @staticmethod
+    def real_time_indicator():
+        return datetime.now(pytz.timezone('Asia/Manila')).strftime('%Y-%m-%d %H:%M:%S')
 
 
 
