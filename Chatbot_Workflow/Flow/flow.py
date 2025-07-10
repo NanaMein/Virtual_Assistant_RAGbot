d21e4bec56ce006d3b0184ef54beeb82f3b2d984
Alfeo The Third <alfeoephraimquintay@gmail.com>
2025-07-10 09:48:20 +0800

    made changes espcially im thinking of using flow being used to other places too

--- Chatbot_Workflow/Flow/flow.py
+++ Chatbot_Workflow/Flow/flow.py
@@ -4,6 +4,9 @@ import asyncio
 from collections import deque
 from functools import lru_cache
 from typing import Deque, Type, Optional
+from groq import AsyncGroq
+from groq.types.chat import ChatCompletionUserMessageParam, ChatCompletionAssistantMessageParam, \
+    ChatCompletionSystemMessageParam
 from llama_index.core.storage.chat_store.base_db import MessageStatus
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.embeddings.cohere import CohereEmbedding
@@ -48,15 +51,84 @@ class FlowStateHandler(BaseModel):
 
 class AgenticWorkflow(Flow[FlowStateHandler]):
 
+    temp_cache = TTLCache(ttl=3600, maxsize=10)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     @start()
     async def start1(self):
+
+
         await asyncio.sleep(2)
 
 
     @listen(start1)
     async def listen1(self):
-        await asyncio.sleep(2)
-        return "Ok"
+        user_chat = ChatCompletionUserMessageParam(
+            content=self.state.user_input_message,
+            role="user"
+        )
+
+        messages = self.cache_me(self.state.user_input_id)
+        messages.append(user_chat)
+
+        client = AsyncGroq(api_key=os.getenv("CLIENT_GROQ_API_1"))
+        completion = await client.chat.completions.create(
+            model="meta-llama/llama-4-scout-17b-16e-instruct",
+            messages=messages,
+            temperature=0.7,
+            max_completion_tokens=8192,
+            top_p=0.95,
+            stream=False,
+            stop=None,
+        )
+        chat = completion.choices[0].message
+        assistant_chat = ChatCompletionAssistantMessageParam(
+            content=chat.content,
+            role="assistant"
+        )
+        max = 11
+        memory = self.cache_me(self.state.user_input_id)
+        while len(memory) > max:
+            self.del_cache()
+
+        memory.append(assistant_chat)
+
+        return chat.content
+
+    def cache_me(self, user_id: str):
+        if user_id in self.temp_cache:
+            return self.temp_cache[user_id]
+        system = ChatCompletionSystemMessageParam(
+            content="""### SYSTEM(PRIMING): You are a roleplaying chatbot. You will
+            act like a professional maid that will follow the young master's order.
+            You can disobey but only in words and not in roleplaying.""",
+            role="system"
+        )
+        self.temp_cache[user_id] = [system]
+        return self.temp_cache[user_id]
+
+    def del_cache(self):
+        to_delete = self.temp_cache[self.state.user_input_id]
+        to_delete.pop(1)
+
+
+
+async def main():
+    while True:
+        input_msg = input(" Write your input: ")
+        if input_msg == "exit":
+            break
+        obj = AgenticWorkflow()
+        messages_input = {
+            "user_input_message":input_msg,
+            "user_input_id":"testing_id"
+        }
+        obj1 = await obj.kickoff_async(inputs=messages_input)
+        print(f"User: {input_msg}")
+        print(f"Assistant: {obj1}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
