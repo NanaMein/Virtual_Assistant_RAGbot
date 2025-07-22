287f601fc9020102c18007fe42edbbcd86c331ce
Alfeo The Third <alfeoephraimquintay@gmail.com>
2025-07-22 21:23:24 +0800

    refactor lock in vector, making sure that app can handle concurrent users even if this is in starting point

--- Chatbot_Workflow/RAG/rag_for_chat_conversation/chat_memory_vector.py
+++ Chatbot_Workflow/RAG/rag_for_chat_conversation/chat_memory_vector.py
@@ -15,51 +15,56 @@ load_dotenv()
 
 
 class GetMilvusVectorStore:
-
-    vector_by_id = TTLCache(maxsize=100, ttl=300)
-    vector_by_collection = TTLCache(maxsize=100, ttl=3600)
+    """
+    Asycncronously make a vector connection to zilliz with error handling, lock and reconnection
+    mechanism by providing a user id to and each user id will have its own vector store
+    for chat conversation. It has a time to live of 15 days so it will be clear when certain
+    time has passed
+    """
+    _vector_by_id = TTLCache(maxsize=100, ttl=300)
+    _vector_by_collection = TTLCache(maxsize=100, ttl=3600)
     _lock = asyncio.Lock()
 
     def __init__(self, input_user_id: str):
-
         self.user_id: str = input_user_id
-        self._resources: Optional[MilvusVectorStore] = None
+        self._vector: Optional[MilvusVectorStore] = None
         self._client: Optional[MilvusClient] = None
         self.collection_name: str = f"Collection_Name_{self.user_id.strip()}_2025"
 
-    # @property
-    async def cached_resource(self) -> MilvusVectorStore:
-        if self._resources is None:
-            self._resources = await self._getting_resource(user_id=self.user_id)
-        return self._resources
 
-    # @property
+
+
+
     async def client_for_vector(self) -> MilvusClient:
         if self._client is None:
-            self._client = await self._milvus_client(self.user_id)
+            self._client = await self._milvus_client(user_id=self.user_id)
         return self._client
 
-    async def _milvus_client(self, user_id: str):
-        if user_id in self.vector_by_collection:
-            return self.vector_by_collection[user_id]
+    async def vector_store(self) -> MilvusVectorStore:
+        if self._vector is None:
+            self._vector = await self._getting_resource(user_id=self.user_id)
+        return self._vector
 
-        # async with self._lock:
-        client = MilvusClient(
-            uri=os.getenv('CLIENT_URI'),
-            token=os.getenv('CLIENT_TOKEN')
-        )
-        self.vector_by_collection[user_id] = client
-        self.vector_by_collection.expire()
-        return client
+    async def _milvus_client(self, user_id: str):
+        if user_id in self._vector_by_collection:
+            return self._vector_by_collection[user_id]
+        async with self._lock:
+            client = MilvusClient(
+                uri=os.getenv('CLIENT_URI'),
+                token=os.getenv('CLIENT_TOKEN')
+            )
+            self._vector_by_collection[user_id] = client
+            self._vector_by_collection.expire()
+            return client
 
     async def _getting_resource(self, user_id: str) -> MilvusVectorStore:
-        if user_id in self.vector_by_id:
-            return self.vector_by_id[user_id]
+        if user_id in self._vector_by_id:
+            return self._vector_by_id[user_id]
 
         async with self._lock:
             client = await self.client_for_vector()
 
-            does_it_exist = client.has_collection(
+            existing_collection = client.has_collection(
                 collection_name=self.collection_name
             )
             vector_store = MilvusVectorStore(
@@ -78,31 +83,33 @@ class GetMilvusVectorStore:
                 hybrid_ranker="RRFRanker",
                 hybrid_ranker_params={"k": 120},
             )
-            if not does_it_exist:
+            if not existing_collection:
                 client.alter_collection_properties(
                     collection_name=self.collection_name,
                     properties={"collection.ttl.seconds": ttl_conversion_to_day(15)}
                 )
 
 
-            self.vector_by_id[user_id] = vector_store
-            self.vector_by_id.expire()
-            return self.vector_by_id[user_id]
+            self._vector_by_id[user_id] = vector_store
+            self._vector_by_id.expire()
+            return self._vector_by_id[user_id]
 
     async def milvus_vector_store(self):
         try:
-            vector_store = await self.cached_resource()
+            vector_store = await self.vector_store()
         except (MilvusException, KeyError) as me:
             print(f"MilvusException, KeyError: {me}")
-            self._resources = None
-            vector_store = await self.cached_resource()
+            self._vector = None
+            self._client = None
+            vector_store = await self.vector_store()
         except (AioRpcError, ImportError, grpc.RpcError, UnboundLocalError) as aie:
             print(f"AioRpcError, ImportError, RpcError, UnboundLocalError: {aie}")
-            self._resources = None
-            vector_store = await self.cached_resource()
+            self._vector = None
+            self._client = None
+            vector_store = await self.vector_store()
         except Exception as e:
             print(f"Unexpected Error: {e}")
-            self._resources = None
+            self._vector = None
             self._client = None
             return None
 
